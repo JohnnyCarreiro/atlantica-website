ed7adb55f4b41206fd1216affd40c3028d0d3adb
Joao Victor <JohnnyCarreiro@Johnnys-Macbook-Pro.local>
2020-11-12 22:08:10 -0300

    a

--- src/pages/servicos.tsx
+++ src/pages/servicos.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 
-import { Container } from '../styles/sobre_styles'
+import { Container } from '../styles/servicos_styles'
 
 import heroImg from '../assets/images/hero.png'
 import bluePrintImg from '../assets/images/bluePrint.jpg'
@@ -45,24 +45,23 @@ const ServicesPage: React.FC = () => {
               <h2>Soluções voltadas para cada porte, seguimento e finalidade de cada empresa</h2>
               <div className="servicesCards">
               <Services 
-              image={smallBusiness}
-              title="Pequenas"
-              text="Hoje um dos maiores gastos, seja qual for o tamanho da sua empresa, são os gatos com energia elétrica. Existimos para que sua empresa passe a investir mais em seus próprios negócios...Conheça mais a respeito da energia solar e como que isso pode ajudar a sua empresa a crescer!" 
-              link="/empresas/pequenas"
-            />
-            <Services 
-              image={mediumBusiness}
-              title="Médias"
-              text="Hoje um dos maiores gastos, seja qual for o tamanho da sua empresa, são os gatos com energia elétrica. Existimos para que sua empresa passe a investir mais em seus próprios negócios...Conheça mais a respeito da energia solar e como que isso pode ajudar a sua empresa a crescer!" 
-              link="/empresas/medias"
-            />
-            <Services 
-              image={bigBusiness}
-              title="Grandes"
-              text="Hoje um dos maiores gastos, seja qual for o tamanho da sua empresa, são os gatos com energia elétrica. Existimos para que sua empresa passe a investir mais em seus próprios negócios ...Conheça mais a respeito da energia solar e como que isso pode ajudar a sua empresa a crescer!" 
-              link="/empresas/grandes"
-            />
-
+                  image={smallBusiness}
+                  title="Pequenas"
+                  text="Hoje um dos maiores gastos, seja qual for o tamanho da sua empresa, são os gatos com energia elétrica. Existimos para que sua empresa passe a investir mais em seus próprios negócios...Conheça mais a respeito da energia solar e como que isso pode ajudar a sua empresa a crescer!" 
+                  link="/empresas/pequenas"
+                />
+                <Services 
+                  image={mediumBusiness}
+                  title="Médias"
+                  text="Hoje um dos maiores gastos, seja qual for o tamanho da sua empresa, são os gatos com energia elétrica. Existimos para que sua empresa passe a investir mais em seus próprios negócios...Conheça mais a respeito da energia solar e como que isso pode ajudar a sua empresa a crescer!" 
+                  link="/empresas/medias"
+                />
+                <Services 
+                  image={bigBusiness}
+                  title="Grandes"
+                  text="Hoje um dos maiores gastos, seja qual for o tamanho da sua empresa, são os gatos com energia elétrica. Existimos para que sua empresa passe a investir mais em seus próprios negócios ...Conheça mais a respeito da energia solar e como que isso pode ajudar a sua empresa a crescer!" 
+                  link="/empresas/grandes"
+                />
               </div>
             </div>
           </div>
